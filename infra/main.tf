bbc4fb26805d365286d73dabf71223768efd9401
Kiran Gade <kbgade.cse@gmail.com>
2025-12-11 20:15:07 +0530

    Use S3 backend for Terraform state

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,13 @@
 terraform {
   required_version = ">= 1.6.0"
+
+   backend "s3" {
+    bucket         = "kiran-tf-state-demo-api"                 # <-- your bucket name
+    key            = "dotnet8-aws-cicd-demo/infra/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"    #<-- your dynamodb name
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
